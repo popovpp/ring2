341dad909fde6b9699a352c6de20dd16637486b8
Pavel <popovpp@yandex.ru>
2021-08-15 19:16:21 +0300

    add thread

--- mc1/sender/views.py
+++ mc1/sender/views.py
@@ -10,13 +10,14 @@ import socket
 from datetime import datetime
 from channels.db import database_sync_to_async
 import asyncio
+import threading
 
 from sender.serializers import MessageSerializer
 from sender.models import Message
 
 
 SESSION_ID = 1
-#DURATION = 10
+DURATION = 10
 
 
 @database_sync_to_async
@@ -26,24 +27,8 @@ def get_SESSION_ID():
     return count
 
 
-class MessageViewSet(ModelViewSet):
-    permission_classes = [AllowAny]
-    serializer_class = MessageSerializer
-    queryset = Message.objects.all().order_by('id')
-    DURATION = 2
-
-    @action(methods=['GET'], detail=False, url_path="start", 
-            url_name="start",
-            permission_classes=[AllowAny])
-    def start(self, request, **kwargs):
-        START_TIME = timezone.now()
-        print('START', START_TIME)
-        try:
-            SESSION_ID = Message.objects.latest('id').session_id + 1
-        except Exception as e:
-            SESSION_ID = 0
-        
-        while (timezone.now()-START_TIME).seconds < self.DURATION:
+def message_session(START_TIME=None, DURATION=None, SESSION_ID=None):
+    while (timezone.now()-START_TIME).seconds < DURATION:
             new_message = {'session_id': SESSION_ID,
                            'MC1_timestamp':str(timezone.now()),
                            'MC2_timestamp':None, 
@@ -57,10 +42,55 @@ class MessageViewSet(ModelViewSet):
             serializer = MessageSerializer(data=instance)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-        count_str = str(Message.objects.filter(session_id=SESSION_ID).count())
-        print('Длительность сеанса:', (timezone.now()-START_TIME).seconds, 'секунд')
-        print('Количество сообщений:', count_str)
-        return Response('STOP ' + str(timezone.now()) + ', ' + count_str, status=status.HTTP_200_OK)
+#            t2 = threading.Thread(target=serializer.save, 
+#                                  args=(), 
+#                                  daemon=True)
+#            t2.start()
+    count_str = str(Message.objects.filter(session_id=SESSION_ID).count())
+    print('Длительность сеанса:', (timezone.now()-START_TIME).seconds, 'секунд')
+    print('Количество сообщений:', count_str)
+
+
+class MessageViewSet(ModelViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all().order_by('id')
+
+    @action(methods=['GET'], detail=False, url_path="start", 
+            url_name="start",
+            permission_classes=[AllowAny])
+    def start(self, request, **kwargs):
+        START_TIME = timezone.now()
+        print('START', START_TIME)
+        try:
+            SESSION_ID = Message.objects.latest('id').session_id + 1
+        except Exception as e:
+            SESSION_ID = 0
+
+        t1 = threading.Thread(target=message_session, 
+                              kwargs=dict(START_TIME=START_TIME, DURATION=DURATION, SESSION_ID=SESSION_ID), 
+                              daemon=True)
+        t1.start()
+        
+#        while (timezone.now()-START_TIME).seconds < self.DURATION:
+#            new_message = {'session_id': SESSION_ID,
+#                           'MC1_timestamp':str(timezone.now()),
+#                           'MC2_timestamp':None, 
+#                           'MC3_timestamp':None,
+#                           'end_timestamp':None}
+#            serializer = MessageSerializer(new_message)
+#            response = requests.post('http://web1:8001/messages/', data=serializer.data)
+#            print(response.json())
+#            instance = response.json()
+#            instance['end_timestamp'] = str(timezone.now())
+#            serializer = MessageSerializer(data=instance)
+#            serializer.is_valid(raise_exception=True)
+#            serializer.save()
+#        count_str = str(Message.objects.filter(session_id=SESSION_ID).count())
+#        print('Длительность сеанса:', (timezone.now()-START_TIME).seconds, 'секунд')
+#        print('Количество сообщений:', count_str)
+#        return Response('STOP ' + str(timezone.now()) + ', ' + count_str, status=status.HTTP_200_OK)
+        return Response('START ' + str(timezone.now()), status=status.HTTP_200_OK)
 
     @action(methods=['GET'], detail=False, url_path="stop", 
             url_name="stop",
